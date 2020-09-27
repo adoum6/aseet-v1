340141e0493579b068e421a149ab68ab2644abaa
Adoum Achekh <adoumachekhmahamat@gmail.com>
2020-09-27 13:55:18 +0000

    Integration for admin template

--- src/Controller/Activity/CategoryController.php
+++ src/Controller/Activity/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Activity;
 
+use App\Entity\Activity\Category;
+use App\Form\Activity\CategoryType;
 use App\Repository\Activity\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -18,6 +21,27 @@ class CategoryController extends AbstractController
     {
         return $this->render('activity/category/index.html.twig', [
             'categories' => $repository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("new", name="category.index")
+     */
+    public function new(Request $request, CategoryRepository $repository)
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() and $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($category);
+            $manager->flush();
+        }
+        return $this->render('activity/category/index.html.twig', [
+            //'categories'    =>  $repository->findAll(),
+            'category'      =>  $category,
+            'form'          =>  $form->createView(),
         ]);
     }
 
